373b5567e5ffa90e0892bae98554fd12272aae7a
bhohnco <bryanhohn13@gmail.com>
2021-07-08 16:29:21 -0600

    Dummy data file add for addresses

---
+++ walk-safe/src/assets/AddressTestingData.js
@@ -0,0 +1,44 @@
+const addressData = [
+  {
+    Address: '6245 Garrison St, Arvada, CO 80004'
+  },
+  {
+    Address: '5700 Balsam St, Arvada, CO 80002'
+  },
+  {
+    Address: '1602 Garfield St, Denver, CO 80206'
+  },
+  {
+    Address: '1626 Steele St, Denver, CO 80206'
+  },
+  {
+    Address: '211 Galapago St, Denver, CO 80223'
+  },
+  {
+    Address: '501 W Cedar Ave, Denver, CO 80223'
+  },
+  {
+    Address: '1555 Court Pl, Denver, CO 80202'
+  },
+  {
+    Address: '1075 N Washington St, Denver, CO 80203'
+  },
+  {
+    Address: '801 Kalamath St, Denver, CO 80204'
+  },
+  {
+    Address: '1212 Mariposa St, Denver, CO 8020'
+  },
+  {
+    Address: '1408 Lowell Blvd, Denver, CO 80204'
+  },
+  {
+    Address: '1300 Osceola St, Denver, CO 80204'
+  },
+  {
+    Address: '1674 Xavier St, Denver, CO 80204'
+  },
+  {
+    Address: '1400 Stuart St, Denver, CO 80204'
+  }
+]
