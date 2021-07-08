3f69c541d6c6dbf75b196c4a3177cebc166a9316
pcmueller <pmuellerleile@gmail.com>
2021-07-08 16:56:21 -0500

    Install Sass and build out HTML boilerplate

--- walk-safe/src/Components/App/App.js
+++ walk-safe/src/Components/App/App.js
@@ -1,23 +1,23 @@
-import logo from '../../logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WALK SAFE
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <section>
+          <form>
+            <button>
+              USERNAME
+            </button>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
