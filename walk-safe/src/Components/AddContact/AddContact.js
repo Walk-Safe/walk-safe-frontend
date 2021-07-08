3d50bddcf0fda06362cfab06da21e7d3d623a9f4
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-08 16:48:49 -0600

    write form jsx

--- walk-safe/src/Components/AddContact/AddContact.js
+++ walk-safe/src/Components/AddContact/AddContact.js
@@ -5,4 +5,25 @@ import './AddContact.scss';
 function AddContact() {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
+
+  return(
+    <form>
+      <div className='form-container'>
+        <h1>Add Contact</h1>
+        <input
+          title='name'
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          title='phone'
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </div>
+      <button onClick={postContact}>Add Contact</button>
+    </form>
+  )
 }
+
+export default AddContact;
