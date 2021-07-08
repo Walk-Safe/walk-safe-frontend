5150b1db140c989093875d677b38b42d2ed9bac2
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-08 16:32:53 -0600

    set form state

---
+++ walk-safe/src/Components/AddContact/AddContact.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import './AddContact.scss';
+
+
+function AddContact() {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+}
