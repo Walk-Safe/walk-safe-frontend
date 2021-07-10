b38f940a97ff46941de97e370bd6d60e0cbf76f6
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-10 10:24:02 -0600

    add contact data

---
+++ walk-safe/src/assets/getContactQuery.js
@@ -0,0 +1,6 @@
+const contactQuery = [
+  {user_id: 1, first_name: "Ashley", last_name: "Childs", phone_number: "+18501234567"},
+  {user_id: 1, first_name: "Matt", last_name: "Childs", phone_number: "+18504567890"},
+  {user_id: 1, first_name: "Serena", last_name: "Williams", phone_number: "+17478763456"},
+  {user_id: 1, first_name: "Caroline", last_name: "Eubanks", phone_number: "+18502124830"}
+]
