8b749e33fb8addffbfda598404e970f2a1efff8d
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-10 15:07:06 -0600

    attempt ref values

--- walk-safe/src/Components/AddContact/AddContact.js
+++ walk-safe/src/Components/AddContact/AddContact.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react';
+import { useMutation } from "@apollo/react-hooks";
 
 
 
 function AddContact() {
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
 
-  /*postContact method here for GraphQL data*/
+  const [addContact] = useMutation()
+
+  /* addContact is our mutate function that gets called when we are ready for the mutation to execute*/
+
+  /* Refs in React give us a means of storing mutable values throughout a component's lifecycle, and are often used for interacting with the DOM without the need of re-rendering a component. In other words, we do not need to rely on state management to update an element with Refs. */
+
+  /*
+
+
+
+
+  */
 
   return(
-    <form className='contact-form'>
+    <form className='contact-form' onSubmit={ e => {
+      e.preventDefault();
+      addContact({ variables: })
+    }}>
       <h1>Add Contact</h1>
       <input
-        title='name'
-        value={name}
-        onChange={(event) => setName(event.target.value)}
+        title='first'
+        value={first}
+      />
+      <input
+        title='last'
+        value={last}
       />
       <input
         title='phone'
         value={phone}
-        onChange={(event) => setPhone(event.target.value)}
       />
-      <button>Add Contact</button>
+      <button type='submit'>Add Contact</button>
     </form>
   )
 }
