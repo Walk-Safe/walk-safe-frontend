0915fb88d72d0b066724e686c70ef5720ef74373
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-10 17:50:50 -0600

    add parameters to useMutation

--- walk-safe/src/Components/AddContact/AddContact.js
+++ walk-safe/src/Components/AddContact/AddContact.js
@@ -4,30 +4,28 @@ import { useMutation } from "@apollo/react-hooks";
 
 /* defining the mutation */
 const ADD_CONTACT = gql`
- mutation addContact($first: String!, $last: String!, $phone: String!) {
-   addContact(first_name: $first, last_name: $last, phone_number: $phone) {
+ mutation addContact($first: String!, $last: String!, $phone: String!, $id: Integer!) {
+   addContact(first_name: $first, last_name: $last, phone_number: $phone, user_id: $id) {
      first_name
      last_name
      phone_number
+     user_id
    }
  }
 `
 
-function AddContact() {
-  const [name, setFirst] = useState('')
-  const [name, setLast] = useState('')
+function AddContact({id}) {
+  const [first, setFirst] = useState('')
+  const [last, setLast] = useState('')
   const [phone, setPhone] = useState('')
-  const [addContact] = useMutation()
+  const [addContact] = useMutation(ADD_CONTACT, { variables: { first, last, phone, id }})
 
   /* addContact is our mutate function that gets called when we are ready for the mutation to execute*/
 
-  /* Refs in React give us a means of storing mutable values throughout a component's lifecycle, and are often used for interacting with the DOM without the need of re-rendering a component. In other words, we do not need to rely on state management to update an element with Refs. */
+  /* Refs in React give us a means of storing mutable values throughout a component's lifecycle, and are often used for interacting with the DOM without the need of re-rendering a component. In other words, we do not need to rely on state management to update an element with Refs. (used when you are not using useState) */
 
   return(
-    <form className='contact-form' onSubmit={ e => {
-      e.preventDefault();
-      addContact({ variables: })
-    }}>
+    <form className='contact-form'>
       <h1>Add Contact</h1>
          <input
            title='first'
@@ -44,7 +42,7 @@ function AddContact() {
            value={phone}
            onChange={(event) => setPhone(event.target.value)}
          />
-      <button type='submit'>Add Contact</button>
+      <button onClick={addContact}>Add Contact</button>
     </form>
   )
 }
