cf17a1c9c21d1ce90d3ec909c152c6c5432c9c65
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-12 16:23:59 -0600

    update greeting with props

--- walk-safe/src/Components/NavBar/NavBar.js
+++ walk-safe/src/Components/NavBar/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function NavBar() {
+function NavBar({user}) {
   return (
     <nav className='navbar'>
       <div className='welcome-container'>
-        <h2 className='welcome-msg'>Welcome, user's name!</h2>
+        <h2 className='welcome-msg'>Welcome, {user}</h2>
       </div>
       <div className='hamburger-container'>
         <i className='fas fa-bars fa-2x' style={{width: '100%', height: '100%'}}></i>
@@ -14,6 +14,5 @@ function NavBar() {
   )
 }
 
-      
+
 export default NavBar;
-      
