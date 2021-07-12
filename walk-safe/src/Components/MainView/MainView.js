f24a43f7cbed0ae4122d3bf4b2754a27e810de91
pcmueller <pmuellerleile@gmail.com>
2021-07-12 11:34:11 -0600

    Restructured component directory

---
+++ walk-safe/src/Components/MainView/MainView.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../NavBar/NavBar';
+import Header from '../Header/Header';
+import Form from '../Form/Form';
+import MapDisplay from '../MapDisplay/MapDisplay';
+
+function MainView() {
+  return (
+    <main className='main-page'>
+      <NavBar />
+      <Header />
+      <Form />
+      <MapDisplay />
+    </main>
+  )
+}
+
+export default MainView;
