8e2084a96eba196658adf04e776922a0ba87222b
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-12 16:02:10 -0600

    define query for oneUser

--- walk-safe/src/Components/MainView/MainView.js
+++ walk-safe/src/Components/MainView/MainView.js
@@ -3,8 +3,28 @@ import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
 import Form from '../Form/Form';
 import MapDisplay from '../MapDisplay/MapDisplay';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_USER = gql`
+query GetUser {
+  oneUser(id: 1) {
+    firstName
+    lastName
+    username
+    contacts {
+      firstName
+      phoneNumber
+    }
+  }
+}
+`
 
 function MainView() {
+ const { loading, error, data } = useQuery(GET_USER);
+
+ if (loading) return 'Loading...';
+ if (error) return `Error! ${error.message}`;
+ if (data) return `${console.log(data)}`;
 
   return (
     <main className='main-page'>
