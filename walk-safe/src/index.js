2add9908aebe6cc7aae456abfd05982f202a91c7
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-12 13:26:34 -0600

    setup apollo and graphql

--- walk-safe/src/index.js
+++ walk-safe/src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './Components/App/App';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-    <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
