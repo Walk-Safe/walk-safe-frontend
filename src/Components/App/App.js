ccd482d919b58ad7dfe88e24aabf44712027d5fd
pcmueller <pmuellerleile@gmail.com>
2021-07-14 22:06:21 -0600

    Adjust background color

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import LoginView from '../LoginView/LoginView';
-import MainView from '../MainView/MainView';
+// import MainView from '../MainView/MainView';
 import TripView from '../TripView/TripView';
 
 function App() {
