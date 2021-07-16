33ce13feff791ec183690ff658a8c230a9e5926a
pcmueller <pmuellerleile@gmail.com>
2021-07-15 22:02:54 -0600

    Revert App to function component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 // import LoginView from '../LoginView/LoginView';
-// import MainView from '../MainView/MainView';
-import TripView from '../TripView/TripView';
+import MainView from '../MainView/MainView';
+// import TripView from '../TripView/TripView';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      eta: '',
-    }
+function App() {
+
+  const [eta, setETA] = useState('');
+
+  const handleEtaChange = (time) => {
+    setETA(time);
   }
 
-  handleEtaChange = (time) => {
-    this.setState({ eta: time })
-  }
-
-  render() {
+  // render() {
     return (
       <div className="App">
         {/* <LoginView /> */}
-        {/* <MainView 
-          handleEtaChange={this.handleEtaChange}
-        /> */}
-        <TripView 
-          eta={this.state.eta}
+        <MainView 
+          handleEtaChange={handleEtaChange}
         />
+        {/* <TripView 
+          eta={eta}
+        /> */}
       </div>
     );
-  }
+  // }
 }
 
 export default App;
