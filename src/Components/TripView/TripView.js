77b3f31a5de65ff4eb60045f37e3d228ece091f7
pcmueller <pmuellerleile@gmail.com>
2021-07-16 23:56:24 -0600

    Synchronize timers

--- src/Components/TripView/TripView.js
+++ src/Components/TripView/TripView.js
@@ -7,6 +7,19 @@ function TripView({ eta }) {
   const [ hours, setHours ] = useState('');
   const [ minutes, setMinutes ] = useState('');
   const [ seconds, setSeconds ] = useState('');
+
+  const minuteSeconds = 60;
+  const hourSeconds = 3600;
+  const daySeconds = 86400;
+
+  const startTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = startTime + 243248; // use UNIX timestamp in seconds
+  const remainingTime = endTime - startTime;
+
+  const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+  const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+  const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
+
   const mockETA = 136;
 
   useEffect(() => {
@@ -23,12 +36,18 @@ function TripView({ eta }) {
 
   const renderTime = (unit, time) => {
     return (
-      <div className='time-wrapper'>
+      <div className='timer-wrapper'>
         <div className='time-amt'>{time}</div>
-        <div>{unit}</div>
+        <div className='time-unit'>{unit}</div>
       </div>
-    );
-};
+    )
+  }
+
+  const timerProps = {
+    isPlaying: true,
+    size: 180,
+    strokeWidth: 6,
+  };
 
   // npm package: https://www.npmjs.com/package/react-countdown-circle-timer
   // Days, hours, minutes, seconds countdown: https://codesandbox.io/s/musing-davinci-mqssz?fontsize=14&hidenavigation=1&theme=dark
@@ -37,30 +56,48 @@ function TripView({ eta }) {
     <main className='trip-page'>
       <Header />
       <section className='trip-container'>
-        <article className='timer-container'>
+        <article className='timers-container'>
           <CountdownCircleTimer
-            className={'hours-timer'}
-            colors={[["#D14081"]]}
-          >
-            {({ remainingTime }) => renderTime('hours', hours)}
-          </CountdownCircleTimer>
-          <CountdownCircleTimer
-            className={'minutes-timer'}
-            colors={[["#EF798A"]]}
-          >
-            {({ remainingTime }) => renderTime('minutes', minutes)}
-          </CountdownCircleTimer>
-          <CountdownCircleTimer
-            isPlaying
-            className={'seconds-timer'}
-            duration={10}
-            colors={[
-              ['#004777', 0.33],
-              ['#F7B801', 0.33],
-              ['#A30000', 0.33],
+            {...timerProps}
+            className={'timer hours-timer'}
+            colors={[["#18F315"]]}
+            duration={daySeconds}
+            initialRemainingTime={remainingTime % daySeconds}
+            onComplete={(totalElapsedTime) => [
+              remainingTime - totalElapsedTime > hourSeconds
             ]}
           >
-            {({ remainingTime }) => renderTime('seconds', seconds)}
+            {({ elapsedTime }) => renderTime('hours', getTimeHours(daySeconds - elapsedTime))}
+          </CountdownCircleTimer>
+          <CountdownCircleTimer
+            {...timerProps}
+            className={'timer minutes-timer'}
+            colors={[["#FA4677"]]}
+            duration={hourSeconds}
+            initialRemainingTime={remainingTime % hourSeconds}
+            onComplete={(totalElapsedTime) => [
+              remainingTime - totalElapsedTime > minuteSeconds
+            ]}
+          >
+            {({ elapsedTime }) => renderTime('minutes', getTimeMinutes(hourSeconds - elapsedTime))}
+          </CountdownCircleTimer>
+          <CountdownCircleTimer
+            {...timerProps}
+            className={'timer seconds-timer'}
+            colors={[
+              ['#4687FA', 0.20],
+              ['#9D46FA', 0.20],
+              ['#FD3023', 0.20],
+              ['#E3FD23', 0.20],
+              ['#0CF3FE', 0.20],
+            ]}
+            duration={minuteSeconds}
+            initialRemainingTime={remainingTime % minuteSeconds}
+            onComplete={(totalElapsedTime) => [
+              remainingTime - totalElapsedTime > 0
+            ]}
+          >
+            {({ elapsedTime }) => renderTime('seconds', getTimeSeconds(elapsedTime))}
           </CountdownCircleTimer>
         </article>
         <button className='end-walk-btn'>
