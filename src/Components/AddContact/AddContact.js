d0b0f5a0d96f08e14fd7f78e5f4ca5bd1b8dff1f
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-17 14:08:53 -0600

    fix phone entry errors

--- src/Components/AddContact/AddContact.js
+++ src/Components/AddContact/AddContact.js
@@ -18,16 +18,23 @@ const CREATE_CONTACT = gql`
 function AddContact() {
   const [firstName, setFirst] = useState('')
   const [lastName, setLast] = useState('')
+  const [countryCode, setCountry] = useState('')
+  const [areaCode, setArea] = useState('')
   const [phoneNumber, setPhone] = useState('')
   const [createContact, { loading: mutationLoading, error: mutationError, data }] = useMutation(CREATE_CONTACT)
 
   function addContact(e) {
     e.preventDefault();
-    createContact( {variables: { firstName: firstName, lastName: lastName, phoneNumber: phoneNumber}});
+    let number = `+${countryCode}${areaCode}${phoneNumber}`;
+    createContact( {variables: { firstName: firstName, lastName: lastName, phoneNumber: number}});
     console.log('data', data);
-    console.log('phone#', phoneNumber);
+    console.log('phone#', number);
     console.log('name', firstName);
     console.log('lastname', lastName);
+  }
+
+  function modifyNumberInput(event) {
+    setPhone(toString(event.target.value));
   }
 
   return(
@@ -35,19 +42,36 @@ function AddContact() {
       <h1>Add Contact</h1>
          <input
            title='firstName'
+           placeholder='First Name'
            value={firstName}
            onChange={(event) => setFirst(event.target.value)}
          />
          <input
            title='lastName'
+           placeholder='Last Name'
            value={lastName}
            onChange={(event) => setLast(event.target.value)}
          />
-         <input
-           title='phoneNumber'
-           value={phoneNumber}
-           onChange={(event) => setPhone(event.target.value)}
-         />
+         <div className='phoneNumber'>
+           <input
+             title='countryCode'
+             placeholder='Country Code'
+             value={countryCode}
+             onChange={(event) => setCountry(event.target.value)}
+           />
+           <input
+             title='areaCode'
+             placeholder='Area Code'
+             value={areaCode}
+             onChange={(event) => setArea(event.target.value)}
+           />
+           <input
+             title='phoneNumber'
+             placeholder='Phone Number'
+             value={phoneNumber}
+             onChange={(event) => setPhone(event.target.value)}
+           />
+         </div>
       {mutationLoading && <p className='loading'>Loading...</p>}
       {mutationError && <p>Error: Please try again</p>}
       <button onClick={addContact}>Add Contact</button>
