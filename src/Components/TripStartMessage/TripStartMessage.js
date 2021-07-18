2af6c41b46d096b1cc75d607564f15c5bcca3bde
bhohnco <bryanhohn13@gmail.com>
2021-07-18 13:44:48 -0600

    Tested and working with contact number being passed down and trip info displaying on text message

--- src/Components/TripStartMessage/TripStartMessage.js
+++ src/Components/TripStartMessage/TripStartMessage.js
@@ -1,32 +1,30 @@
-import React from 'react';
 
-function TripStartMessage( {tripInfo, contactName, userNameInfo} ) {
+function TripStartMessage (tripDetails, contact, userName) {
+   console.log(tripDetails)
+    let textInformation = tripDetails.createTrip.trip
+   console.log(textInformation)
+    const sendSms = () => {
 
- let textInformation = tripInfo.createTrip.trip
+      let smsObj = {
+        mobile_number: `${contact.phone}`,
+        message: `${userName.firstName} has started a trip from ${textInformation.startPoint} to ${textInformation.endPoint}. ${userName.firstName} is traveling by ${textInformation.travelMode} with an ETA of ${textInformation.eta} minutes. Please be on the look out for a follow up message from ${userName.firstName}`,
+      }
 
-  const sendSms = (event) => {
-    event.preventDefault();
-
-    let smsObj = {
-      mobile_number: `${contactName.phone}`,
-      message: `${userNameInfo.firstName} has started a trip from ${textInformation.startPoint} to ${textInformation.endPoint}. ${userNameInfo.firstName} is traveling by ${textInformation.travelMode} with an ETA of ${textInformation.eta} minutes. Please be on the look out for a follow up message from ${userNameInfo.firstName}`,
+      fetch('https://walk-safe-backend.herokuapp.com/sms_messages', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          accepts: "application/json"
+        },
+        body: JSON.stringify(smsObj)
+      })
+          .then(result => result.text())
+          .then(resp => console.log(resp))
     }
 
-    fetch('https://walk-safe-backend.herokuapp.com/sms_messages', {
-      method:'POST',
-      headers: {
-        'content-type': 'application/json',
-        accepts: "application/json"
-      },
-      body: JSON.stringify(smsObj)
-    })
-        .then(result => result.text())
-        .then(resp => console.log(resp))
-  }
-
-  return (
-          <button onClick={sendSms}>Submit</button>
-  );
+    return(
+  sendSms()
+)
 }
 
 export default TripStartMessage;
