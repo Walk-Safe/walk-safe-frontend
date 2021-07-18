405dfd696e9fdb0a685c8ebd3036e08ae55f0f45
bhohnco <bryanhohn13@gmail.com>
2021-07-17 20:37:17 -0600

    Removed unused code

--- src/Components/TripStartMessage/TripStartMessage.js
+++ src/Components/TripStartMessage/TripStartMessage.js
@@ -1,26 +1,16 @@
 import React from 'react';
 
-function TripStartMessage( user, eta ) {
-  // const [sms, setSms] = useState('');
-  // const [contactNumber, setContactNumber] = useState('');
+function TripStartMessage( {tripInfo, contactName, userNameInfo} ) {
+
+ let textInformation = tripInfo.createTrip.trip
 
   const sendSms = (event) => {
     event.preventDefault();
 
     let smsObj = {
-      mobile_number: '17083630654',
-      message: 'This text indicates that we have one badass Back-End team!',
+      mobile_number: `${contactName.phone}`,
+      message: `${userNameInfo.firstName} has started a trip from ${textInformation.startPoint} to ${textInformation.endPoint}. ${userNameInfo.firstName} is traveling by ${textInformation.travelMode} with an ETA of ${textInformation.eta} minutes. Please be on the look out for a follow up message from ${userNameInfo.firstName}`,
     }
-
-    // let smsObjTripEnd = {
-    //   mobile_number: '1' + contactNumber,
-    //   message: sms,
-    // }
-    //
-    // let smsObjTripNotComplete = {
-    //   mobile_number: '1' + contactNumber,
-    //   message: sms,
-    // }
 
     fetch('https://walk-safe-backend.herokuapp.com/sms_messages', {
       method:'POST',
@@ -39,4 +29,4 @@ function TripStartMessage( user, eta ) {
   );
 }
 
-  export default TripStartMessage;
+export default TripStartMessage;
