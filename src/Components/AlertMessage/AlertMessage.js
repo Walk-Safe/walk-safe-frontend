728c7f3c0b5aa154c43142869112527fef0e8eb2
pcmueller <pmuellerleile@gmail.com>
2021-07-18 17:57:31 -0600

    Complete modal buildout

--- src/Components/AlertMessage/AlertMessage.js
+++ src/Components/AlertMessage/AlertMessage.js
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactModal from 'react-modal';
 import { NavLink } from 'react-router-dom';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import alertModalStyles from './jsxStyles/alertModalStyles';
 
 ReactModal.setAppElement('#root');
 
-function AlertMessage({ modalIsOpen, closeModal }) {
+function AlertMessage({ setEmergency, modalIsOpen, closeModal }) {
 
-  function handleExitBtnClick() {
+  function handleModalClose() {
+    setEmergency(false);
     closeModal();
   }
+
+  const timerProps = {
+    isPlaying: true,
+    size: 30,
+    strokeWidth: 3,
+  };
 
   return (
     <ReactModal
@@ -19,15 +27,25 @@ function AlertMessage({ modalIsOpen, closeModal }) {
       contentLabel='alert modal'
       preventScroll={true}
     >
-      <div className='alert-modal'>
-        <NavLink className='alert-exit-link' exact to='/'>
-          <i type='button' onClick={handleExitBtnClick} className="fas fa-times fa-2x alert-exit-btn"></i>
-        </NavLink>
+      <section className='alert-modal'>
+        <div className='alert-nav'>
+          <CountdownCircleTimer
+            {...timerProps}
+            className={'timer alert-timer'}
+            colors={[['#000', 100]]}
+            duration={10}
+            onComplete={handleModalClose}
+          >
+          </CountdownCircleTimer>
+          <NavLink className='alert-exit-link' exact to='/'>
+            <i type='button' onClick={handleModalClose} className="fas fa-times fa-2x alert-exit-btn"></i>
+          </NavLink>
+        </div>
         <h2 className='alert-title'>!ALERT!</h2>
         <p className='alert-message'>
           Your contact has been notified that your trip was not completed in time.
         </p>
-      </div>
+      </section>
     </ReactModal>
   )
 }
