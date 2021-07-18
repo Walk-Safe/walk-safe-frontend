225ecbdeda5bd2a0a7e18ddb215b5bff7e378c42
Caroline Eubanks <cmeubanks@gmail.com>
2021-07-18 13:41:19 -0600

    update fixture data

--- cypress/integration/addcontact.js
+++ cypress/integration/addcontact.js
@@ -1,36 +1,31 @@
 import { aliasQuery, aliasMutation } from '../utils/graphql-test-utils';
-import userData from "../fixtures/user-data.json";
+// import userData from "../fixtures/user-data.json";
 
 describe ('Add Contacts Page View', () => {
 
   beforeEach(() => {
-    cy.intercept('POST', 'http://localhost:3000/graphql', (req) => {
-      aliasQuery(req, 'GetUser')
-
-      aliasMutation(req, 'CreateContact')
-    })
+    cy.intercept('POST', 'https://walk-safe-backend.herokuapp.com/graphql', { fixture:'user-data.json'})
   })
 
-
   it('should display Header when you visit the add contact page', () => {
-    cy.intercept('POST', 'http://localhost:3000/graphql', (req) => {
-      const { body } = req
-      if (hasOperationName(req, 'GetUser')) {
-        // Declare the alias from the initial intercept in the beforeEach
-        req.alias = 'gqlGetUserQuery'
-
-        // Set req.fixture or use req.reply to modify portions of the response
-        req.reply((res) => {
-          // Modify the response body directly
-            res.body.data = {
-                  userData,
-            }
-        })
-      }
-    })
+    // cy.intercept('POST', 'http://localhost:3000/graphql', (req) => {
+    //   const { body } = req
+    //   if (hasOperationName(req, 'GetUser')) {
+    //     // Declare the alias from the initial intercept in the beforeEach
+    //     req.alias = 'gqlGetUserQuery'
+    //
+    //     // Set req.fixture or use req.reply to modify portions of the response
+    //     req.reply((res) => {
+    //       // Modify the response body directly
+    //         res.body.data = {
+    //               userData
+    //         }
+    //     })
+    //   }
+    // })
 
     // Must visit after cy.intercept
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
     cy.url().should('eq', 'http://localhost:3000/')
 
   })
