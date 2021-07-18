3cc75c3fc12292493b0b00a71d80c97733c4cebf
pcmueller <pmuellerleile@gmail.com>
2021-07-18 17:35:45 -0600

    Complete initial modal styling

---
+++ src/Components/AlertMessage/jsxStyles/alertModalStyles.js
@@ -0,0 +1,18 @@
+const alertModalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    height: '50%',
+    width: '50%',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'ghostwhite',
+    overflow: 'visible',
+    position: 'absolute',
+    border: '1px solid #bcc5ca'
+  },
+};
+
+export default alertModalStyles;
