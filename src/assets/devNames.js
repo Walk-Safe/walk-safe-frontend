dba9c0967d9e83416fb3b38fecc6dd514ceffdfa
pcmueller <pmuellerleile@gmail.com>
2021-07-19 15:35:48 -0600

    Link GitHub accounts to dev cards

---
+++ src/assets/devNames.js
@@ -0,0 +1,10 @@
+const devNames = [
+  { fullName: 'Caroline Eubanks', ghName: 'cmeubanks', id: '73092355'},
+  { fullName: 'Bryan Hohn', ghName: 'bhohnco', id: '71860165'},
+  { fullName: 'Peter Muellerleile', ghName: 'pcmueller', id: '51062974'},
+  { fullName: 'Gaelyn Cooper', ghName: 'gaelyn', id: '57960885'},
+  { fullName: 'Mike Foy', ghName: 'foymikek', id: '56099485'},
+  { fullName: 'Joe Mecha', ghName: 'joemecha', id: '72046344'}
+]
+
+export default devNames;
