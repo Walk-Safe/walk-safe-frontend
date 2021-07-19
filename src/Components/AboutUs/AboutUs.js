4e434a06260536ce399cbc2b14d2dde80994a5c6
pcmueller <pmuellerleile@gmail.com>
2021-07-19 15:35:48 -0600

    Link GitHub accounts to dev cards

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -1,8 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
+import devNames from '../../assets/devNames';
 
 function AboutUs({ user }) {
+
+  let [ feDevCards, setFEDevCards ] = useState([]);
+  let [ beDevCards, setBEDevCards ] = useState([]);
+
+  function buildDevCard(dev) {
+    return (
+      <article className='dev-card'>
+        <div class="github-card user-card">
+          <div class="header User"></div>
+          <a class="avatar" href={`https://github.com/${dev.ghName}`} target="_top">
+            <img src={`https://avatars.githubusercontent.com/u/${dev.id}?v=4&amp;s=80`} alt={dev.fullName}/>
+          </a>
+          <div class="content">
+            <a href={`https://github.com/${dev.ghName}?tab=repositories`} target="_top">
+              <h1>{dev.fullName}</h1>
+            </a>
+          </div>
+        </div>
+      </article>
+    )
+  }
+
+  useEffect(() => {
+    // if (devNames) {
+      populateCards();
+    // }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function populateCards() {
+    const feCards = devNames.map(dev => {
+      if (devNames.indexOf(dev) < 3) {
+        return buildDevCard(dev);
+      }
+    });
+    const beCards = devNames.map(dev => {
+      if (devNames.indexOf(dev) > 2)
+      return buildDevCard(dev);
+    });
+    setFEDevCards(feCards);
+    setBEDevCards(beCards);
+  }
+
+  if (beDevCards.length === 0) return <p className='loading'>Loading...</p>;
 
   return (
     <main className='about-us-page'>
@@ -14,24 +59,10 @@ function AboutUs({ user }) {
         </p>
       </div>
         <section className='team-grid'>
-          <article className='dev-card'>
-            TEAM MEMBER #1
-          </article>
-          <article className='dev-card'>
-            TEAM MEMBER #2
-          </article>
-          <article className='dev-card'>
-            TEAM MEMBER #3
-          </article>
-          <article className='dev-card'>
-            TEAM MEMBER #4
-          </article>
-          <article className='dev-card'>
-            TEAM MEMBER #5
-          </article>
-          <article className='dev-card'>
-            TEAM MEMBER #6
-          </article>
+          <h3>Front End</h3>
+          {feDevCards}
+          <h3>Back End</h3>
+          {beDevCards}
         </section>
     </main>
   )
