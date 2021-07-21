cdb52380473e0db5ca7cf9b8947db32ab6bdc07f
pcmueller <pmuellerleile@gmail.com>
2021-07-21 11:44:19 -0600

    Complete media query adjustments for main page

--- src/Components/NavBar/mediaQueries.js
+++ src/Components/NavBar/mediaQueries.js
@@ -1,6 +1,10 @@
 export default function DropdownWidthMediaQuery(width) {
   if (width < 360) {
-    return '80%';
+    return '70%';
+  } if (width < 651) {
+    return '50%';
+  } if (width < 1025)  {
+    return '35%';
   } else {
     return '20%';
   }
