30b063674a0fbc4fb81869e1bc0a5b039b3c5f66
pcmueller <pmuellerleile@gmail.com>
2021-07-29 07:08:42 -0600

    Adjust media queries

--- src/Components/AddContact/AddContact.js
+++ src/Components/AddContact/AddContact.js
@@ -76,12 +76,13 @@ function AddContact({ user }) {
 
   return (
     <section className='add-contact'>
-      {user && <NavBar nameToggle='true' user={user.firstName}/>}
+      <NavBar nameToggle='true' user={user.firstName}/>
       <Header />
       <form className='contact-form'>
         {valid && <p className='form-error'>Complete Fields With Valid Data</p>}
         {verify && <p className='form-error'>Enter valid phone number</p>}
         <h2>Add Contact</h2>
+          <div className='contact-name'>
            <input
              title='firstName'
              placeholder='First Name'
@@ -94,6 +95,7 @@ function AddContact({ user }) {
              value={lastName}
              onChange={(event) => setLast(event.target.value)}
            />
+          </div>
            <h3>Phone Number</h3>
            <div className='phone-number'>
              <input
