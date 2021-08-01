bd7f93e760d8ab8b4430d9f2d32518e694b3809d
pcmueller <pmuellerleile@gmail.com>
2021-08-01 16:47:15 -0600

    Complete Alert modal responsiveness

--- src/Components/CurrentTrip/CurrentTrip.js
+++ src/Components/CurrentTrip/CurrentTrip.js
@@ -108,7 +108,7 @@ function CurrentTrip({ user, eta, contact, tripIsActive, setTripIsActive, switch
     setMinutesActive(true);
     setSecondsActive(true);
     setExtensionModalIsOpen(false);
-    setAlertModalIsOpen(false);
+    setAlertModalIsOpen(true);
   }
 
   function endTrip() {
