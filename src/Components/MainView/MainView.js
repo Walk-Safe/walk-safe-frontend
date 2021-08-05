b742b7ac772f5f6efab0864791b7d758c8c3e3a4
pcmueller <pmuellerleile@gmail.com>
2021-08-05 11:53:56 -0600

    Move GET_USER query to App.js

--- src/Components/MainView/MainView.js
+++ src/Components/MainView/MainView.js
@@ -1,55 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Header from '../Header/Header';
 import Form from '../Form/Form';
-import { gql, useQuery } from '@apollo/client';
 
-const GET_USER = gql`
-query GetUser {
-  oneUser(id: 2) {
-    firstName
-    lastName
-    username
-    contacts {
-      firstName
-      lastName
-      phoneNumber
-    }
-  }
-}
-`
 
-function MainView({ setCurrentUser, setCurrentContact, handleEtaChange, setTripIsActive, switchTheme }) {
-  
- const { loading, error, data } = useQuery(GET_USER);
+function MainView ({ loading, error, currentUser, setCurrentContact, handleEtaChange, setTripIsActive, switchTheme }) {
 
-useEffect(() => {
-  if (data) {
-    setCurrentUser(data.oneUser);
-  }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [data]);
-
-if (loading) return (
-  <main className='main-page'>
-    <p className='loading'>Loading...</p>
-  </main>
-);
-
-if (error) return `Error! ${error.message}`;
-  return (
+  if (loading) return (
     <main className='main-page'>
-      {data && <NavBar nameToggle='true' user={data.oneUser.firstName} switchTheme={switchTheme} />}
-      <Header />
-      {data && <Form
-        contacts={data.oneUser.contacts}
-        userInfo={data.oneUser}
-        handleEtaChange={handleEtaChange}
-        setContact={setCurrentContact}
-        setTripIsActive={setTripIsActive}
-      />}
+      <p className='loading'>Loading...</p>
     </main>
-  )
+  );
+
+  if (error) return `Error! ${error.message}`;
+    return (
+      <main className='main-page'>
+        {currentUser && <NavBar nameToggle='true' user={currentUser.firstName} switchTheme={switchTheme} />}
+        <Header />
+        {currentUser && <Form
+          contacts={currentUser.contacts}
+          userInfo={currentUser}
+          handleEtaChange={handleEtaChange}
+          setContact={setCurrentContact}
+          setTripIsActive={setTripIsActive}
+        />}
+      </main>
+    )
 }
 
 
