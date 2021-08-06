5c742d8c33d950754face3200d127c8ad0fab084
pcmueller <pmuellerleile@gmail.com>
2021-08-05 19:18:16 -0600

    Complete AddContact input text responsiveness

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme, GlobalStyles } from '../../theme';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
